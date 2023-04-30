b4b738a3efb9662f8cde00a591f6f9a118e2fb7a
AlexFT257 <ftb2570@gmail.com>
2023-04-30 01:43:16 -0400

    busqueda del socket, sistema de mensjaes

--- Server/server.cpp
+++ Server/server.cpp
@@ -9,67 +9,111 @@
 #include <netdb.h>
 #include <ctime>
 
-//#include "server.h"
+#include "server.h"
 
 using namespace std;
 
 // Función para manejar la jugada del jugador
-void handle_play(int client_sockfd) {
+void handle_play(int client_sockfd)
+{
     // Código para manejar la jugada del jugador
-    cout << "Conexion establecida con "<< client_sockfd << endl;
+    cout << "Conexion establecida con: " << client_sockfd << endl;
 }
 
-int main() {
+// funcion para controlar el usuario
+void handle_user(int client_sockfd)
+{
+    while (true)
+    {
+        char buffer[1024];
+        int bytes = recv(client_sockfd, buffer, sizeof(buffer), 0);
+
+        if (bytes < 0)
+        {
+            cerr << "Error al recibir el mensaje" << endl;
+            exit(EXIT_FAILURE);
+        }
+
+        // procesar el mensaje
+        string message = string(buffer, bytes);
+        cout << "Mensaje recibido: " << message << endl;
+
+        // enviar un mensaje de respuesta
+        string response = "Hola cliente";
+        send(client_sockfd, response.c_str(), response.length(), 0);
+
+        // cerrar la conexion si el cliente envia "exit"
+        if (message == "exit")
+        {
+            break;
+        }
+    }
+    close(client_sockfd);
+    exit(EXIT_SUCCESS);
+}
+
+int main()
+{
     // Se crea el socket del servidor
+    int server_port = find_port(5000);
+
     int server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_sockfd < 0) {
-        cerr << "Error al abrir el socket del servidor" << endl;
+    if (server_sockfd < 0)
+    {
+        cerr << "Error al crear el socket del servidor" << endl;
         exit(EXIT_FAILURE);
     }
 
-    // Se especifica la dirección del servidor
+    // Se configura la dirección del servidor
     struct sockaddr_in server_address;
     server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(5000); // Puerto del servidor
+    server_address.sin_port = htons(server_port);
     server_address.sin_addr.s_addr = INADDR_ANY;
 
     // Se vincula el socket a la dirección del servidor
-    if (bind(server_sockfd, (struct sockaddr *) &server_address, sizeof(server_address)) < 0) {
+    if (bind(server_sockfd, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
+    {
         cerr << "Error al vincular el socket del servidor a la dirección" << endl;
         exit(EXIT_FAILURE);
     }
 
     // Se pone el socket en modo de escucha
-    if (listen(server_sockfd, 5) < 0) {
+    if (listen(server_sockfd, 5) < 0)
+    {
         cerr << "Error al poner el socket en modo de escucha" << endl;
         exit(EXIT_FAILURE);
     }
 
     cout << "Servidor iniciado en el puerto " << ntohs(server_address.sin_port) << endl;
 
-    while (true) {
+    while (true)
+    {
         // Se acepta una conexión entrante de un cliente
         int client_sockfd = accept(server_sockfd, NULL, NULL);
-        if (client_sockfd < 0) {
+        if (client_sockfd < 0)
+        {
             cerr << "Error al aceptar la conexión entrante del cliente" << endl;
             continue;
         }
 
         // Se crea un nuevo proceso para manejar la jugada del jugador
         pid_t pid = fork();
-        cout<<"pid de conexion: "<< pid<< endl;
-        if (pid < 0) {
+        cout << "pid de conexion: " << pid << endl;
+        if (pid < 0)
+        {
             cerr << "Error al crear el proceso hijo" << endl;
             close(client_sockfd);
             continue;
-        } else if (pid == 0) {
+        }
+        else if (pid == 0)
+        {
             // Este es el proceso hijo
             close(server_sockfd);
-            handle_play(client_sockfd);
 
-            
+            handle_user(client_sockfd);
+
             // close(client_sockfd);
-            exit(EXIT_SUCCESS);
+            // exit(EXIT_SUCCESS);
         }
     }
 
--- Server/server.h
+++ Server/server.h
@@ -10,28 +10,30 @@
 #include <condition_variable>
 
 // constantes del juego
-const BOARD_SIZE = 15;
+const int BOARD_SIZE = 15;
 const int NUM_SHIPS = 9; // 1 portaaviones, 2 buques, 2 submarinos, 3 lanchas
 
 
-
 // representacion de una posicion en el tablero
-struct Position {
+struct Position
+{
     int x;
     int y;
 };
 
 // representacion de un barco
-struct Ship {
+struct Ship
+{
     std::vector<Position> positions;
     char type; // 'P' = portaaviones, 'B' = buque, 'S' = submarino, 'L' = lancha
-}
+};
 
 // representacion de un jugador
-struct Player {
+struct Player
+{
     int id;
     int socket_fd;
-}
+};
 
 // representacion de un juego
 struct Game
@@ -44,3 +46,40 @@ struct Game
 };
 
 
+// variables globales
+std::vector<Game> games;
+std::mutex games_mutex;
+std::condition_variable games_cv;
+
+// funciones
+int find_port(int initial_port);
+
+int find_port(int initial_port){
+    int port = initial_port;
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        std::cerr << "Error al crear el socket del servidor" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(port);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port=htons(port);
+
+    // intenta vincular el socket a la direccion del servidor
+    int bind_result = bind(sockfd, (struct sockaddr *)&server_address, sizeof(server_address));
+    while (bind_result < 0)
+    {
+        port++;
+        server_address.sin_port = htons(port);
+        bind_result = bind(sockfd, (struct sockaddr *)&server_address, sizeof(server_address));
+    }
+
+    // cuando se logra vincular el socket a la direccion del servidor, se cierra el socket
+    // y se retorna el puerto para abrir el socket del servidor en el main
+    close(sockfd);
+    return port;
+}
